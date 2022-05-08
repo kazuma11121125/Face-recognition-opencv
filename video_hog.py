8a419e3b8ce940e1dcce6242b7e17d12722d4a9e
kazuma11121125 <kazuma1112.bakabaka.ahoaho@gmail.com>
2022-05-08 19:44:41 +0900

    修正

--- video_hog.py
+++ video_hog.py
@@ -31,7 +31,7 @@ writer = cv2.VideoWriter(PATHS[1],fourcc, fps, saizu)
 cascade = cv2.CascadeClassifier(XML_PATH)
 img1 = cv2.imread(PATHS[2])
 rects1 = cascade.detectMultiScale(img1, scaleFactor=1.2, minNeighbors=2, minSize=(1, 1))
-print('[cnn] 検出された対象人物人数: {}'.format(len(rects1)))
+print('[hog] 検出された対象人物人数: {}'.format(len(rects1)))
 
 # 画質変換
 while True:
@@ -57,7 +57,7 @@ global img_array
 img_array = []
 while (cap20.isOpened()):
     frame_sec = frame_sec+1
-    print(f"現在{frame_sec}\n残り{frame_sec_all-frame_sec}\n{frame_sec/frame_sec_all*100}%完了")
+    print(f"[hog] 現在{frame_sec}\n[hog]残り{frame_sec_all-frame_sec}\n[hog]{frame_sec/frame_sec_all*100}%完了")
     se = se+1
     ret1, frame = cap20.read()
     print(ret1)
@@ -66,11 +66,11 @@ while (cap20.isOpened()):
     for x, y, w, h in face:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
     rects = cascade.detectMultiScale(frame, scaleFactor=1.2, minNeighbors=2, minSize=(1, 1))
-    print('[cnn] 検出された人数: {}'.format(len(rects)))
+    print('[hog] 検出された人数: {}'.format(len(rects)))
     ho = len(rects)
     if ho ==0:
-        cv2.imwrite(f"Analysis_result_cnn/none/Analysis_result{i}{a}{se}{noe}.jpg",frame)
-        img = cv2.imread(f"Analysis_result_cnn/none/Analysis_result{i}{a}{se}{noe}.jpg")
+        cv2.imwrite(f"Analysis_result_hog/none/Analysis_result{i}{a}{se}{noe}.jpg",frame)
+        img = cv2.imread(f"Analysis_result_hog/none/Analysis_result{i}{a}{se}{noe}.jpg")
         noe = noe+1
         img_array.append(img)
     if ho > 0:
@@ -79,15 +79,15 @@ while (cap20.isOpened()):
         rects1 = cascade.detectMultiScale(img1, scaleFactor=1.2, minNeighbors=2, minSize=(1, 1))
         y = len(rects1)
         if y == 0:
-            print("[cnn] 対象人物が検出されませんでした")
+            print("[hog] 対象人物が検出されませんでした")
             break
         if y >0:
-            print(f"[cnn] 対象人物 {y}人")
+            print(f"[hog] 対象人物 {y}人")
             for i in range(ho):
-                print(f"[cnn] 検出された {i+1}人目")
+                print(f"[hog] 検出された {i+1}人目")
                 try:
                     for a in range(y):
-                        print(f"[cnn] 対象人物 {y}人目")
+                        print(f"[hog] 対象人物 {y}人目")
                         # step1 画像読み込みとコンバート
                         img_elon = frame
                         img_test = face_recognition.load_image_file(PATHS[2])
@@ -95,10 +95,10 @@ while (cap20.isOpened()):
                         # step2 顔認証
                         face_loc = face_recognition.face_locations(img_elon)[i]
                         # 128次元の顔エンコーディングのリスト
-                        encode_elon = face_recognition.face_encodings(img_elon,model="cnn")[i]
+                        encode_elon = face_recognition.face_encodings(img_elon,model="hog")[i]
                         cv2.rectangle(img_elon, (face_loc[3], face_loc[0]), (face_loc[1], face_loc[2]), (255, 0, 255), 2)
                         face_loc_test = face_recognition.face_locations(img_test)[a]
-                        encode_elon_test = face_recognition.face_encodings(img_test,model="cnn")[a]
+                        encode_elon_test = face_recognition.face_encodings(img_test,model="hog")[a]
 
                         # print(encode_elon_test)
                         cv2.rectangle(img_test, (face_loc_test[3], face_loc_test[0]), (face_loc_test[1], face_loc_test[2]), (255, 0, 255), 2)
@@ -107,10 +107,10 @@ while (cap20.isOpened()):
                         results = face_recognition.compare_faces([encode_elon], encode_elon_test)
                         # 値が小さい程マッチしている
                         face_dis = face_recognition.face_distance([encode_elon], encode_elon_test)
-                        print(f"[cnn] {results, face_dis}")
+                        print(f"[hog] {results, face_dis}")
                         p = 1 - face_dis[0]
                         probability = p*100
-                        print(f"[cnn] {probability}%")
+                        print(f"[hog] {probability}%")
                         if results == [False]:
                             res = "No match."
                         elif results == [True]:
@@ -119,17 +119,17 @@ while (cap20.isOpened()):
                             elif probability > 60:
                                 res = "Match found."
                         text = f"{res}:{probability}%"
-                        print(text)
-                        resu = f'result_cnn/{i}{a}{se}.jpg'
-                        resu1 = f'result_cnn/{i}{a}{se}_tm.jpg'
+                        print(f"[hog]{text}")
+                        resu = f'result_hog/{i}{a}{se}.jpg'
+                        resu1 = f'result_hog/{i}{a}{se}_tm.jpg'
                         cv2.imwrite(resu, img_elon)
                         cv2.imwrite(resu1, img_test)
                         pic = Image.open(resu)
                         pic1 = Image.open(resu1)
                         img_resize = pic.resize((1000, 800))
                         img_resize1 = pic1.resize((400, 300))
-                        reze = f'result_cnn/{i}{a}{se}_ts.jpg'
-                        reze1 = f'result_cnn/{i}{a}{se}_tm_ts.jpg'
+                        reze = f'result_hog/{i}{a}{se}_ts.jpg'
+                        reze1 = f'result_hog/{i}{a}{se}_tm_ts.jpg'
                         img_resize.save(reze)
                         img_resize1.save(reze1)
 
@@ -153,29 +153,29 @@ while (cap20.isOpened()):
                             )
 
                         # 保存する
-                        graw = f'result_cnn/Analysis_result{i}{a}{se}.jpg'
+                        graw = f'result_hog/Analysis_result{i}{a}{se}.jpg'
                         photo_img.save(graw)
                         im1 = Image.open(graw)
                         im2 = Image.open(reze1)
-                        completion = f'Analysis_result_cnn/Analysis_result{i}{a}{se}.jpg'
+                        completion = f'Analysis_result_hog/Analysis_result{i}{a}{se}.jpg'
                         back_im = im1.copy()
                         back_im.paste(im2, (590, 530))
                         back_im.save(completion, quality=95)
 
-                        img = cv2.imread(f'Analysis_result_cnn/Analysis_result{i}{a}{se}.jpg')
+                        img = cv2.imread(f'Analysis_result_hog/Analysis_result{i}{a}{se}.jpg')
                         img_array.append(img)
                 except Exception as e:
                     try:
-                        cv2.imwrite(f"Analysis_result_cnn/none/Analysis_result{i}{a}{se}{noe}.jpg",frame)
-                        img = cv2.imread(f"Analysis_result_cnn/none/Analysis_result{i}{a}{se}{noe}.jpg")
+                        cv2.imwrite(f"Analysis_result_hog/none/Analysis_result{i}{a}{se}{noe}.jpg",frame)
+                        img = cv2.imread(f"Analysis_result_hog/none/Analysis_result{i}{a}{se}{noe}.jpg")
                         img_array.append(img)
                         noe = noe+1
                     except:
-                        print("[cnn] error" + str(e))
-                    print("[cnn] error" + str(e))
+                        print("[hog] error" + str(e))
+                    print("[hog] error" + str(e))
     
 
-name = 'Face recognition cnn.mp4'
+name = 'Face recognition hog.mp4'
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 video = cv2.VideoWriter(name,fourcc,10,(1000, 800))
 
